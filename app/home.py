ba14aa23b5c9e8d8d65100fdbe8fc76cd550519c
Rachel A <RachelAlcraft@gmail.com>
2023-11-12 16:37:38 +0000

    typo

--- app/home.py
+++ app/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
         page_title="ra-box",
-        page_icon="app/static/ed-icon.png"i
+        page_icon="app/static/ed-icon.png",
         layout="wide",
     )
 
