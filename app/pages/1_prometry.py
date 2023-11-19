e0f4166aa420b927e04f375faed6e079015fa9f5
RachelAlcraft <rachelalcraft@gmail.com>
2023-11-19 12:16:01 +0000

    Improved samples and documentation

--- app/pages/1_prometry.py
+++ app/pages/1_prometry.py
@@ -4,7 +4,7 @@ from prometry import pdbgeometry as pg
 import pandas as pd
 import plotly.express as px
 
-DATADIR = "app/static/"
+DATADIR = "app/data/"
 
 is_calc = False
 
@@ -15,14 +15,80 @@ st.set_page_config(
     )
 
 
-st.header("prometry")
+code_string = "from prometry import pdbloader as pl\n"
+code_string += "from prometry import pdbgeometry as pg\n"
+code_string += "import pandas as pd\n"
+code_string += f"DATADIR = {DATADIR}\n"
+
+st.header("PROMETRY")
 st.write("#### A library to calculate geometric parameters of protein structures and perform criteria search")
 
+
+
+with st.expander("Expand for geo and criteria description help"):
+    st.code("""
+        ---------------------------------------------------------------------       
+        Examples
+        ---------------------------------------------------------------------  
+        Lengths in the backbone: N:CA CA:C C:N+1 C:O  
+        Backbone angle tau: N:CA:C  
+        Phi and psi : C-1:N:CA:C and N:CA:C:N+1                  
+        The carbonyl oxygen and the nearest N to it that is not in the same residue: O:(N&1) 
+        The disulfide bonds (or nearest cysteine sulphurs): SG:(SG&1)  
+        Any O or N within 2.5-3 of the N but not the same residue: N:{(O),(N)@1}[dis|2.5<>3.0]
+        ---------------------------------------------------------------------  
+        Description
+        ---------------------------------------------------------------------  
+        With no brackets or symbols, the simple case is that you are looking for atom types within a residue  
+        N:CA - is looking for all atoms of type N and the CA in the same residue, for the distance  
+        N:CA:C - is looking for all atoms of types N, CA and C in the same residue and the angle  
+
+        +/- look for neighbouring residues
+        C:N+1 - looks for the C in a residue and the N in the next residue  
+        C-1:N - as per above but the previous residue to the current   
+        ---------------------------------------------------------------------  
+        () brackets symbolise element rather than atom type, eg N can mean NZ, NE1 etc, and this starts a 
+        nearest lookup if it is not the first item, or a cross product if it is
+        (N):N - means any type N, and the N in the same residue - there can be more than 1 per residue
+        N:(N) - means all N's in a reside and the nearest type N in the same residue - only 1 per residue
+        ---------------------------------------------------------------------  
+        {} symbolise distance searchers - the list of atoms is all the candidates
+        N:{O,N} is all N's in a residue and the nearest O or N to it - 1 per residue
+        ---------------------------------------------------------------------  
+        () brackets can be used
+        N:{(O),(N)} is all N's in a residue and the nearest O type or N type to it - 1 per residue
+        ---------------------------------------------------------------------  
+        operators @ or & specify x nearest or at least x away as follows
+        N:{O,N@1} means N and the second nearest  O or N to it - 0 indexed
+        N:{O,N&2} means N and the nearest O or N as long as it is at least 2 residues away
+        ---------------------------------------------------------------------  
+        [] after a geo specify a comma delim list of criteria
+        aa - amino acid of the residue
+        dis - distance from the first atom where
+        < less than
+        > greater than
+        <> between
+        >< extremes
+        occ - occupancy, with just =, < or >
+        ---------------------------------------------------------------------  
+    """)
+
+
+if 'pdbs' not in st.session_state:
+    st.session_state['pdbs'] = ""
+if 'geos' not in st.session_state:
+    st.session_state['geos'] = ""
+
 data_samples = {}
-data_samples["crambin"] = ("1crn 1ejg 3u7t 2fd7 1cbn 1cnr 3nir 1ab1 2fd9 1jxy 1jxu 1jxx 1jxw 1jxt",
-                            "CA:C:N+1 N:O N:CA:C:N+1 C:O N:CA N:CA:C",
-                            "N:CA","resolution","pdb_code",
+data_samples["crambin resolution"] = ("1crn 1ejg 3u7t 2fd7 1cbn 1cnr 3nir 1ab1 2fd9 1jxy 1jxu 1jxx 1jxw 1jxt",
+                            "N:CA CA:C C:O C:N+1",
+                            "N:CA","C:O","resolution",
                             "rid","bf_N:CA","pdb_code")
+
+data_samples["crambin disulfide"] = ("1crn 1ejg 3u7t 2fd7 1cbn 1cnr 3nir 1ab1 2fd9 1jxy 1jxu 1jxx 1jxw 1jxt",
+                            "SG:{SG@1} SG:N SG:O N:CA",
+                            "SG:{SG@1}","info_SG:{SG@1}","pdb_code",
+                            "SG:{SG@1}","SG:O","pdb_code")
 
 data_samples["brca1"] = ("AF-Q8CGX5-F1-model_v4",
                             "N:CA:C:N+1 C-1:N:CA:C N:CA CA:C C:O",
@@ -34,21 +100,15 @@ data_samples["4rek"] = ("4rek",
                             "N:CA:C:N+1","N:N+1","N:CA:C",
                             "C-1:N:CA:C","N:CA:C:N+1","N:CA:C")
 
-data_samples["iron"] = ("5d8v 4u9h 5jsk 6rk0",
-                            "FE:{(N),(O)} FE:{(N),(O)@2} FE:{(N),(O)&2} (FE):(O,N)+1",
-                            "FE:{(N),(O)}","FE:{(N),(O)@2}","aa",
-                            "FE:{(N),(O)&2}","(FE):(O,N)+1","pdb_code")
-
-data_samples["sulphur contacts"] = ("1AB1 1AHO 1CBN 1DY5 1EJG 1ETL 1ETM 1ETN 1F94 1FN8 1FY4 1FY5 1G4I 1G66 1G6X 4NDS 4NSV 4R5R 4UNU 4UYR 4WKA 4XDX 4XOJ 4Y9W 4YEO 4ZM7 5A71 5AVD 5AVG 5DJ7 5DK1 5DKM 5E7W 5E9N 5HMV 5HQI 5I5B",
-                            #"SG:{SG@1} SG:{(N),(O)}[dis|<3] SG:{(N),(O)}:{SG@1}",
-                            "SG:{SG@1} SG:{(N),(O)} SG:{(N),(O)}:{SG@1}",
-                            "SG:{SG@1}","SG:{(N),(O)}","SG:{(N),(O)}:{SG@1}",
-                            "SG:{(N),(O)}:{SG@1}","SG:{SG@1}","SG:{(N),(O)}")
+data_samples["iron"] = ("2d5x 2peg 3vrg 1j41 1j40 3gkv 2r80 2d5z 1thb 2dn2 2dn1 2dn3 2w72 6rp5 2h8f 3d1k 3bom 4esa 5eui 6zmx 6ihx 6ii1 7dy3 7dy4 1uiw 6kah 6kai 6ka9 6kae 6lcx 6lcw 6l5w 6kao 6kaq 6kap 6l5v 1bab 1bz0 1ird 3s66 7jy3",
+                            "FE:{(N),(O)} FE:{(N),(O)@1} FE:{(N),(O)@2} FE:{(N),(O)@3}",
+                            "FE:{(N),(O)}","FE:{(N),(O)@1}","aa",
+                            "FE:{(N),(O)@2}","FE:{(N),(O)@3}","pdb_code")
 
 data_samples["hydrogen bonds"] = ("1AB1 1AHO 1CBN 1DY5 1EJG 1ETL 1ETM 1ETN 1F94 1FN8 1FY4 1FY5 1G4I 1G66 1G6X 4NDS 4NSV 4R5R 4UNU 4UYR 4WKA 4XDX 4XOJ 4Y9W 4YEO 4ZM7 5A71 5AVD 5AVG 5DJ7 5DK1 5DKM 5E7W 5E9N 5HMV 5HQI 5I5B",
-                            "O:{(N)}[dis|2.0<>3.00] C:O N:CA:C N:CA",                            
-                            "O:{(N)}[dis|2.0<>3.00]","info_O:{(N)}[dis|2.0<>3.00]","C:O",
-                            "O:{(N)}[dis|2.0<>3.00]","C:O","N:CA:C")
+                            "N:{(O),(N)&1}[dis|2.5<>3.0] C:O N:CA:C N:CA",                            
+                            "N:{(O),(N)&1}[dis|2.5<>3.0]","info_N:{(O),(N)&1}[dis|2.5<>3.0]","pdb_code",
+                            "N:{(O),(N)&1}[dis|2.5<>3.0]","info_N:{(O),(N)&1}[dis|2.5<>3.0]","aa")
 
 
 
@@ -81,67 +141,102 @@ df = pd.DataFrame({'A' : []})
 if 'data' not in st.session_state:
     st.session_state['data'] = df
 else:
-    df = st.session_state['data']
+    if st.session_state['pdbs'] != structures or st.session_state['geos'] != geos:
+        st.session_state['data'] = df
+        st.session_state['pdbs'] = structures
+        st.session_state['geos'] = geos
+    else:
+        df = st.session_state['data']
 
-#if len(df.index) > 0:
-#    st.dataframe(df)
 
 if st.button("Calulate dataframe"):
+
+    code_string += f"ls_structures = {ls_structures}\n"
+    code_string += f"ls_geos = {ls_geos}\n"
     
     pobjs = []
-    for pdb in ls_structures:
+    code_string += f"for pdb in ls_structures:"
+    code_string += """    
+    if "AF-" in pdb:
+        source = "alphafold"
+    else:
         source = "ebi"
+        pdb = pdb.lower()
+    pla = pl.PdbLoader(pdb,DATADIR,cif=False,source=source)        
+    po = pla.load_pdb()
+    pobjs.append(po)
+    """
+    
+    
+    for pdb in ls_structures:        
         if "AF-" in pdb:
             source = "alphafold"
-        if source == "ebi":
+        else:
+            source = "ebi"
             pdb = pdb.lower()
-        pla = pl.PdbLoader(pdb,DATADIR,cif=False,source=source)
+        pla = pl.PdbLoader(pdb,DATADIR,cif=False,source=source)        
         po = pla.load_pdb()
         pobjs.append(po)
+    
     gm = pg.GeometryMaker(pobjs)
     df = gm.calculateGeometry(ls_geos)
 
-    #st.dataframe(df)
-    st.session_state['data'] = df
-    
-
-
+    code_string += "\ngm = pg.GeometryMaker(pobjs)\n"
+    code_string += "df = gm.calculateGeometry(ls_geos)\n"
+        
+    st.session_state['data'] = df    
+    st.session_state['code_df'] = code_string
+            
 if len(df.index) > 0:
     st.dataframe(df)
+    st.download_button("Download promerty file",df.to_csv(),"promerty.csv","text/csv",key='download-csv')
+
+    with st.expander("Expand for code to replicate dataframe generation"):
+        st.code(st.session_state['code_df'])
+    
     ax_cols = list(df.columns)
-    iidx1 = ax_cols.index(idx1)
-    iidy1 = ax_cols.index(idy1)
-    iidz1 = ax_cols.index(idz1)
-    iidx2 = ax_cols.index(idx2)
-    iidy2 = ax_cols.index(idy2)
-    iidz2 = ax_cols.index(idz2)
+    iidx1,iidy1,iidz1,iidx2,iidy2,iidz2 = 0,0,0,0,0,0
+    try:
+        iidx1 = ax_cols.index(idx1)
+        iidy1 = ax_cols.index(idy1)
+        iidz1 = ax_cols.index(idz1)
+        iidx2 = ax_cols.index(idx2)
+        iidy2 = ax_cols.index(idy2)
+        iidz2 = ax_cols.index(idz2)
+    except:
+        pass
+
      
 
     cols = st.columns(6)
     with cols[0]:
-        x_ax1 = st.selectbox("Choose x-axis 1", ax_cols,index=iidx1)
+        x_ax1 = st.selectbox("x-axis 1", ax_cols,index=iidx1)
     with cols[1]:
-        y_ax1 = st.selectbox("Choose y-axis 1",ax_cols,index=iidy1)
+        y_ax1 = st.selectbox("y-axis 1",ax_cols,index=iidy1)
     with cols[2]:
-        z_ax1 = st.selectbox("Choose z-axis (hue) 1",ax_cols,index=iidz1)
+        z_ax1 = st.selectbox("z-axis (hue) 1",ax_cols,index=iidz1)
     with cols[3]:
-        x_ax2 = st.selectbox("Choose x-axis 2", ax_cols,index=iidx2)
+        x_ax2 = st.selectbox("x-axis 2", ax_cols,index=iidx2)
     with cols[4]:
-        y_ax2 = st.selectbox("Choose y-axis 2",ax_cols,index=iidy2)
+        y_ax2 = st.selectbox("y-axis 2",ax_cols,index=iidy2)
     with cols[5]:
-        z_ax2 = st.selectbox("Choose z-axis (hue) 2",ax_cols,index=iidz2)
+        z_ax2 = st.selectbox("z-axis (hue) 2",ax_cols,index=iidz2)
 
     if st.button("Calulate plot"):
         cols = st.columns(2)
         with cols[0]:
-            fig = px.scatter(df, x=x_ax1, y=y_ax1, color=z_ax1,title="",width=600, height=600, opacity=0.7)
+            fig = px.scatter(df, x=x_ax1, y=y_ax1, color=z_ax1,title="",width=500, height=500, opacity=0.7,color_continuous_scale=px.colors.sequential.Viridis)
             st.plotly_chart(fig, use_container_width=False)
         with cols[1]:
-            fig = px.scatter(df, x=x_ax2, y=y_ax2, color=z_ax2,title="",width=600, height=600, opacity=0.7)
+            fig = px.scatter(df, x=x_ax2, y=y_ax2, color=z_ax2,title="",width=500, height=500, opacity=0.7,color_continuous_scale=px.colors.sequential.Viridis)
             st.plotly_chart(fig, use_container_width=False)
 
-    
-
+        code_string = "import plotly.express as px\n"
+        code_string += f"fig = px.scatter(df, x='{x_ax1}', y='{y_ax1}', color='{z_ax1}',title="",width=500, height=500, opacity=0.7, color_continuous_scale=px.colors.sequential.Viridis))\n"
+        code_string += "fig.show() #or preferred method, e.g. fig.write_html('path/to/file.html')"
+                    
+        with st.expander("Expand for code to replicate plot"):
+            st.code(code_string)
 
     
 
